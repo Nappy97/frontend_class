3018ffd8d632be79da88546c2a4480889dcf5bbd
Nappy <wjddmlgud829@gmail.com>
2022-04-10 13:55:49 +0900

    hi

---
+++ React/day02/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import './indext.css';
+import App from "./App";
+
+ReactDom.render{
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+    document.getElementById()
+}
